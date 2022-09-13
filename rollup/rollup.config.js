df1771fb5f39b15b5afd496f68d0d06f73b1675c
Theodoros Antoniou <theodoros.antoniou@staizen.com>
2022-09-13 17:07:12 +0300

    updated rollup

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,7 +1,8 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import del from 'rollup-plugin-delete';
-import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 import copy from 'rollup-plugin-copy';
+// import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+// import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 
 
 module.exports = {
@@ -16,13 +17,13 @@ module.exports = {
     copy({
       targets: [
         { src: 'src/index.html', dest: 'dist/' },
-        // WORKAROUND 1, static import
+        // WORKAROUND for Bug 1, dist-custom-elements
         { src: 'node_modules/stencil-workers/dist/cjs/*.worker*.js', dest: 'dist/assets' }
-        // WORKAROUND 1, dynamic import
-        // { src: 'node_modules/stencil-workers/dist/cjs/*.worker*.js', dest: 'dist/assets' }
+        // WORKAROUND for Bug 1, lazy loader
+        // { src: 'node_modules/stencil-workers/dist/cjs/*.worker*.js', dest: 'dist' }
       ]
     }),
-    // WORKAROUND 2
+    // WORKAROUND for Bug 2
     // dynamicImportVars(),
   ]
 };
--- rollup/src/index.js
+++ rollup/src/index.js
@@ -1,9 +1,14 @@
-import { defineCustomElements } from 'stencil-workers';
-defineCustomElements();
+// Using default dist-custom-elements
+// Bug 1. worker files need to be manually copied in `dist/assets`
 
-// when using dynamic imports, worker files need to be in `dist/` instead of `dist/assets/`
-// (async function() {
-//   const { defineCustomElements } = await import('stencil-workers/loader');
-//   defineCustomElements();
-// })()
+import { defineCustomElementMyComponent } from 'stencil-workers';
+defineCustomElementMyComponent();
+
+// Using lazy loader
+// Bug 1. worker files need to be manually copied in `dist/` instead of `dist/assets/`
+// Bug 2. also worker files use webpack's dynamic import syntax which allows variables
+//    in order for rollup to support that, we need to use another
+
+// const { defineCustomElements } = await import('stencil-workers/loader');
+// defineCustomElements();
 
