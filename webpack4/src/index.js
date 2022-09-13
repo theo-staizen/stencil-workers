988006b277691247fdaa43c55b16f4917aeea575
Theodoros Antoniou <theodoros.antoniou@staizen.com>
2022-09-13 14:44:49 +0300

    remove webpack4 example

--- webpack4/src/index.js
+++
@@ -1,6 +0,0 @@
-import { defineCustomElements } from 'stencil-workers/loader';
-
-// WORKAROUND: COMMENT-OUT ABOVE, UNCOMMEND BELOW AND ALSO IN webpack.config.js
-// import { defineCustomElements } from 'stencil-workers/loader/index.cjs';
-
-defineCustomElements();
