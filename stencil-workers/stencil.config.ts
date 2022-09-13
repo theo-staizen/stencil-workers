f656caf8b52d5f77b7ebf9195a0517f7835e01c3
Theodoros Antoniou <theodoros.antoniou@staizen.com>
2022-09-13 14:46:04 +0300

    update to latest stencil

--- stencil-workers/stencil.config.ts
+++ stencil-workers/stencil.config.ts
@@ -2,13 +2,17 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'workers',
+  extras: {
+    experimentalImportInjection: true
+  },
   outputTargets: [
     {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
     {
-      type: 'dist-custom-elements-bundle',
+      type: 'dist-custom-elements',
+      generateTypeDeclarations: true
     },
     {
       type: 'docs-readme',
